204ec043e15aedb8f6122ab123400de6d21567fb
Eleonilson <eleonilson.r.l@gmail.com.br>
2024-10-28 00:34:41 -0300

    Initial commit

---
+++ src/test/java/com/eleonilson/SantanderDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleonilson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
