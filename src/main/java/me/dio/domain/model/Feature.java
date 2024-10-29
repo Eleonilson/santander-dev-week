f78bb0977be57c9e0d60c0ce1ebeb8efdd050f26
Eleonilson <eleonilson.r.l@gmail.com.br>
2024-10-29 00:37:22 -0300

    Fazendo o mapeamento da classe Feature.

--- src/main/java/me/dio/domain/model/Feature.java
+++ src/main/java/me/dio/domain/model/Feature.java
@@ -1,8 +1,26 @@
 package me.dio.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_feature")
 public class Feature {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String icon;
     private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getIcon() {
         return icon;
